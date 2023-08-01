5f1fb15d8a072d2b6194e80341a155999242de69
naphomet <141176529+naphomet@users.noreply.github.com>
2023-08-01 11:32:21 -0700

    Add files via upload

---
+++ j-library-summon-js.js
@@ -0,0 +1,6 @@
+<script>
+      function display() {
+         document.getElementById("logo").style.width = "250px";
+         document.getElementById("logo").style.height = "50px";
+      }
+</script>
